c755001442c8b5c2ccac54315f9129e6028c2221
ASMAABDULSAMATHE <asmaabdulsamathe@gmail.com>
2026-05-24 21:51:44 +0400

    Test updated

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 def test_model(model,new_land_property):
+    #Model testing
     new_land_df=pd.DataFrame(new_land_property)
+    new_land_df = new_land_df[model.feature_names_in_]
     new_land_predicted_value=model.predict(new_land_df)
     print("Predicted price:",new_land_predicted_value)
 
